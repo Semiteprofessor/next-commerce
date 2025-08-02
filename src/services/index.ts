9fe1a64c7acfb71441020353c997a632da16cddf
Taiwo Olapade <semiteprofessor@gmail.com>
2025-08-02 18:00:18 +0100

    Commit

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,14 @@
+import { AddBrandPayload, AdminDashboardAnalytics, UpdateBrandPayload } from "../types/admin";
 import { AuthResponse } from "../types/auth";
+import { Brand } from "../types/brand";
 import { ForgetPasswordPayload } from "../types/forgot-password";
 import { LoginPayload } from "../types/login";
 import { ResendOTPPayload, VerifyOTPPayload } from "../types/otp";
 import { RegisterPayload, RegisterResponse } from "../types/register";
-import { ResetPasswordPayload, ResetPasswordResponse } from "../types/reset-password";
+import {
+  ResetPasswordPayload,
+  ResetPasswordResponse,
+} from "../types/reset-password";
 import http from "./http";
 
 export const register = async (
@@ -42,7 +47,6 @@ export const forgetPassword = async (
   return data;
 };
 
-
 export const resetPassword = async (
   payload: ResetPasswordPayload
 ): Promise<ResetPasswordResponse> => {
@@ -69,11 +73,60 @@ export const getNotifications = async (
   );
   return data;
 };
-export const getProducts = async <T = any>(
-  query = "",
-  category?: string,
-  rate?: number
-): Promise<T> => {
-  const { data } = await http.get<T>(`/products${query || "?"}&rate=${rate}`);
+
+
+export const getBrandsByAdmin = async (
+  page: number,
+  search?: string
+): Promise<Brand[]> => {
+  const { data } = await http.get<Brand[]>(
+    `/admin/brands?search=${search || ""}&page=${page}`
+  );
   return data;
 };
+
+export const getBrandByAdmin = async (id: string): Promise<Brand> => {
+  const { data } = await http.get<Brand>(`/admin/brands/${id}`);
+  return data;
+};
+
+export const getAllBrandsByAdmin = async (): Promise<Brand[]> => {
+  const { data } = await http.get<Brand[]>(`/admin/all-brands`);
+  return data;
+};
+
+export const addBrandByAdmin = async (
+  payload: AddBrandPayload
+): Promise<Brand> => {
+  const { data } = await http.post<Brand>(`/admin/brands`, payload);
+  return data;
+};
+
+export const updateBrandByAdmin = async (
+  payload: UpdateBrandPayload
+): Promise<Brand> => {
+  const { currentSlug, ...others } = payload;
+  const { data } = await http.put<Brand>(
+    `/admin/brands/${currentSlug}`,
+    others
+  );
+  return data;
+};
+
+export const deleteBrandByAdmin = async (
+  slug: string
+): Promise<{ message: string }> => {
+  const { data } = await http.delete<{ message: string }>(
+    `/admin/brands/${slug}`
+  );
+  return data;
+};
+
+// export const getProducts = async <T = any>(
+//   query = "",
+//   category?: string,
+//   rate?: number
+// ): Promise<T> => {
+//   const { data } = await http.get<T>(`/products${query || "?"}&rate=${rate}`);
+//   return data;
+// };
