3654cb57220963e5bb0b68604ac0fd71f0b00a5a
Taiwo Olapade <semiteprofessor@gmail.com>
2025-08-02 18:01:34 +0100

    Commit

--- src/types/admin/index.ts
+++ src/types/admin/index.ts
@@ -36,3 +36,73 @@ export interface AddBrandPayload {
 export interface UpdateBrandPayload extends Partial<AddBrandPayload> {
   currentSlug: string;
 }
+
+export interface Category {
+  _id: string;
+  name: string;
+  slug: string;
+  cover?: {
+    _id: string;
+    url: string;
+    blurDataURL: string;
+  };
+  description?: string;
+  metaTitle?: string;
+  metaDescription?: string;
+  status?: string;
+  subCategories?: SubCategory[];
+}
+
+export interface SubCategory {
+  _id: string;
+  name: string;
+  slug: string;
+  category: string; // category id
+  cover?: {
+    _id: string;
+    url: string;
+    blurDataURL: string;
+  };
+  description?: string;
+  metaTitle?: string;
+  metaDescription?: string;
+  status?: string;
+}
+
+export interface AddCategoryPayload {
+  name: string;
+  slug: string;
+  cover?: {
+    _id: string;
+    url: string;
+    blurDataURL: string;
+  };
+  description?: string;
+  metaTitle?: string;
+  metaDescription?: string;
+  status?: string;
+}
+
+export interface UpdateCategoryPayload extends Partial<AddCategoryPayload> {
+  currentSlug: string;
+}
+
+export interface AddSubCategoryPayload {
+  name: string;
+  slug: string;
+  category: string;
+  cover?: {
+    _id: string;
+    url: string;
+    blurDataURL: string;
+  };
+  description?: string;
+  metaTitle?: string;
+  metaDescription?: string;
+  status?: string;
+}
+
+export interface UpdateSubCategoryPayload
+  extends Partial<AddSubCategoryPayload> {
+  currentSlug: string;
+}
